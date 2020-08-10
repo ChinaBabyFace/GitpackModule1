c3f6dbf443621e341a8c0812d7c32af007ce91e6
renyuxiang <inankai@sina.com>
2020-08-10 12:50:10 +0800

    增加日志依赖

--- library/src/main/java/com/shark/gitpack/module1/CommonUtils.java
+++ library/src/main/java/com/shark/gitpack/module1/CommonUtils.java
@@ -1,7 +1,10 @@
 package com.shark.gitpack.module1;
 
+import com.shark.gitpack.module2.SLog;
+
 public class CommonUtils {
     public static String getTag() {
+        SLog.e("CommonUtils","md2");
         return "shark";
     }
 }
